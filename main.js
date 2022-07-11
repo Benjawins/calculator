9541c35d12db30d6eec8b23ff6c01dfa90defe50
Benjawins <osuperpanda@gmail.com>
2022-07-11 11:42:44 +0200

    added operate function
    takes operator and the two numbers and using
    switch case call one of the four math functions.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// the math functions 
 function add (num1, num2) {
   return num1 + num2;
 }
@@ -12,4 +13,24 @@ function multiply (num1, num2) {
 
 function divide (num1, num2) {
   return num1 / num2;
+}
+
+let operator = '';
+
+function operate(operator, num1, num2) {
+  
+  switch(operator){
+    case '+':
+      return add(num1, num2);
+      break;
+    case '-':
+      return subtract(num1, num2);
+      break;
+    case '*':
+        return multiply(num1, num2);
+        break;
+    case '/':
+        return divide(num1, num2);
+        break;
+  }
 }
